d980caa6231fdc6fcec98ea465f1debaa7495f78
semajame <jamesanquillano@gmail.com>
2025-01-04 16:11:54 +0800

    added icon and image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
 
   googleFonts: {
     families: {
